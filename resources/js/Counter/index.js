e1dea99f05a45314bf3878ac8d13739f2546345e
Patricio <ijpatricio@gmail.com>
2025-02-09 00:45:24 +0000

    wip

---
+++ resources/js/Counter/index.js
@@ -0,0 +1,4 @@
+import mingle from '@mingle/mingleVue'
+import Counter from './Counter.vue'
+
+mingle('resources/js/Counter/index.js', Counter)
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@mingle": path.resolve(__dirname, "/vendor/ijpatricio/mingle/resources/js"),
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/filament/admin/theme.css',
+            ],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
