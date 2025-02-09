b435f0a5848546415d9a23c5a0b7d5dbad85729e
Patricio <ijpatricio@gmail.com>
2025-02-09 13:10:42 +0000

    wip

--- resources/js/Talk/Talk.vue
+++ resources/js/Talk/Talk.vue
@@ -11,7 +11,7 @@
             ></textarea>
         </div>
 
-        <div class="flex flex-col gap-2">
+        <div class="mt-2 flex flex-col gap-2">
             <div class="mr-10">
                 Use cached response
                 <input v-model="useCache" type="checkbox" checked="checked" class="toggle toggle-sm"/>
