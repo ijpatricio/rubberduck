1cbb0e7f68c9048116dfb9ddfe7ea828b270c699
Patricio <ijpatricio@gmail.com>
2025-02-14 13:01:14 +0000

    Add debug modal

--- resources/js/Talk/Talk.vue
+++ resources/js/Talk/Talk.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="mt-4 flex justify-end">
             <div class="flex gap-2">
-                <div>System prompt ready: </div>
+                <div>System prompt ready:</div>
                 <span v-if="chatStore.payload.system.length > 0"> ✅ </span>
                 <span v-else> ❌ </span>
             </div>
@@ -26,25 +26,66 @@
 
         <div class="mt-2 mb-8 flex justify-end">
             <button class="btn btn-xs btn-link" @click="savePayload"> Save Payload to localstorage</button>
-            <button class="btn btn-xs btn-link" @click="loadPayload"> Load Payload </button>
+            <button class="btn btn-xs btn-link" @click="loadPayload"> Load Payload</button>
+            <button class="btn btn-xs btn-link" onclick="debug_values.showModal()">Debug raw values</button>
         </div>
-
-        <h1 class="mt-8 mb-4 text-xl font-bold">New message</h1>
 
         <!-- Debug purposes -->
-        <div v-if="1">
-            <pre v-text="chatStore.payload"></pre>
+        <dialog id="debug_values" class="modal">
+            <div class="modal-box w-11/12 max-w-5xl">
+                <h1 class="text-xl font-bold">Debug Raw values</h1>
+                <p class="py-4">Press ESC key or click the button below to close</p>
 
-            <!-- Prompt text previews -->
-            <div class="mt-8">
-                <div>System Prompt:</div>
-                <textarea class="p-4" readonly disabled cols="80" rows="20">{{ chatStore.payload.system }}</textarea>
+
+                <h2 class="text-lg font-bold">System Prompt</h2>
+                <div class="mt-4">
+                    <textarea
+                        class="p-4"
+                        readonly
+                        disabled
+                        cols="80"
+                        rows="20"
+                        v-text="chatStore.payload.system"
+                    ></textarea>
+                </div>
+
+                <h2 class="text-lg font-bold">Messages</h2>
+                <div v-for="message in chatStore.payload.messages" >
+                    <div :class="message.role === 'user' ? 'text-right' : 'text-left'">
+                        <textarea
+                            v-for="content in message.content"
+                            class="p-4"
+                            :class="message.role === 'user' ? 'bg-blue-800' : 'bg-gray-700'"
+                            readonly
+                            disabled
+                            cols="80"
+                            rows="20"
+                            v-text="content.text"
+                        ></textarea>
+                    </div>
+                </div>
+
+                <h2 class="mt-16 text-lg font-bold">RAW Payload</h2>
+                <textarea
+                    class="p-4"
+                    readonly
+                    disabled
+                    cols="80"
+                    rows="20"
+                    v-text="chatStore.payload"
+                ></textarea>
+
+
+                <div class="modal-action">
+                    <form method="dialog">
+                        <!-- if there is a button in form, it will close the modal -->
+                        <button class="btn">Close</button>
+                    </form>
+                </div>
             </div>
-            <div class="flex justify-end">
-                <textarea v-for="message in chatStore.payload.messages" class="p-4" readonly disabled cols="80"
-                          rows="20">{{ message }}</textarea>
-            </div>
-        </div>
+        </dialog>
+
+        <h1 class="mt-8 mb-4 text-xl font-bold">New message</h1>
     </div>
 </template>
 
